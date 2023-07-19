99f3464b4612655d59f73f3f1d9c060e5526f0aa
Selvaganapathi-A <selvaganapathi066@gmail.com>
2023-07-19 16:14:54 +0530

    Add project examples

---
+++ python message encrypt, decrypt.py
@@ -0,0 +1,15 @@
+import rsa
+
+
+public_key, private_key = rsa.newkeys(128)
+message = "Hello"
+encrypted_message = rsa.encrypt(message.encode(), public_key)
+
+decrypted_message = rsa.decrypt(encrypted_message, private_key).decode()
+
+print(encrypted_message)
+print(decrypted_message)
+
+n = 128
+max_message_len_in_bytes = (n // 8) - 11
+print(max_message_len_in_bytes)
